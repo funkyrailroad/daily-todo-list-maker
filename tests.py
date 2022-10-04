54b5c065408c465214f1e302290ded6f2c35fad6
Jake Atwell <jatwell@alexinc.com>
2022-10-04 13:44:10 -0400

    test_weekly_frequency_class_variables

--- tests.py
+++ tests.py
@@ -6,6 +6,7 @@ import main as m
 
 
 class Tests(unittest.TestCase):
+
     def setUp(self):
         self.today = dt.date.today()
         self.tomorrow = self.today + dt.timedelta(days=1)
@@ -19,6 +20,23 @@ class Tests(unittest.TestCase):
         do_tomorrow = freq.do_on_day(self.tomorrow)
 
         self.assertNotEqual(do_today, do_tomorrow)
+
+    def test_weekly_frequency_class_variables(self):
+        monday = dt.date(2022, 10, 3)
+        tuesday = dt.date(2022, 10, 4)
+        wednesday = dt.date(2022, 10, 5)
+        thursday = dt.date(2022, 10, 6)
+        friday = dt.date(2022, 10, 7)
+        saturday = dt.date(2022, 10, 8)
+        sunday = dt.date(2022, 10, 9)
+
+        self.assertEqual(monday.weekday(), f.WeeklyFrequency.MONDAY)
+        self.assertEqual(tuesday.weekday(), f.WeeklyFrequency.TUESDAY)
+        self.assertEqual(wednesday.weekday(), f.WeeklyFrequency.WEDNESDAY)
+        self.assertEqual(thursday.weekday(), f.WeeklyFrequency.THURSDAY)
+        self.assertEqual(friday.weekday(), f.WeeklyFrequency.FRIDAY)
+        self.assertEqual(saturday.weekday(), f.WeeklyFrequency.SATURDAY)
+        self.assertEqual(sunday.weekday(), f.WeeklyFrequency.SUNDAY)
 
     def test_weekday(self):
         wednesday_freq = f.WeeklyFrequency(f.WeeklyFrequency.WEDNESDAY)
