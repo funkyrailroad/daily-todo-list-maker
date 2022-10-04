99fa9aa3eb3a20a7f2dd8e25649773339e8661e4
Jake Atwell <jatwell@alexinc.com>
2022-10-04 11:51:25 -0400

    Test case refactor

--- tests.py
+++ tests.py
@@ -9,9 +9,11 @@ class Tests(unittest.TestCase):
     def setUp(self):
         self.today = dt.date.today()
         self.tomorrow = self.today + dt.timedelta(days=1)
+        self.every_other_freq = f.DailyFrequency(2)
+        self.every_day_freq = f.DailyFrequency(1)
 
     def test_every_other_day_frequency(self):
-        freq = f.DailyFrequency(2)
+        freq = self.every_other_freq
 
         do_today = freq.do_today()
         do_tomorrow = freq.do_on_day(self.tomorrow)
@@ -19,8 +21,7 @@ class Tests(unittest.TestCase):
         self.assertNotEqual(do_today, do_tomorrow)
 
     def test_every_other_day_task(self):
-        freq = f.DailyFrequency(2)
-        task = m.Task("Play Drums", freq)
+        task = m.Task("Play Drums", self.every_other_freq)
 
         do_today = task.do_today()
         do_tomorrow = task.do_on_day(self.tomorrow)
